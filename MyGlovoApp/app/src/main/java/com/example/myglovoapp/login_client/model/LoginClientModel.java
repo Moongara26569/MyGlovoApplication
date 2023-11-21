e6247b3386c2acc5988eb5e3fb0a77cec6036cf7
Moongara26569 <a26569@svalero.com>
2023-11-21 09:27:13 +0100

    conexion db

--- MyGlovoApp/app/src/main/java/com/example/myglovoapp/login_client/model/LoginClientModel.java
+++ MyGlovoApp/app/src/main/java/com/example/myglovoapp/login_client/model/LoginClientModel.java
@@ -6,13 +6,19 @@ import com.example.myglovoapp.beans.Cliente;
 import com.example.myglovoapp.login_client.ContractLoginClient;
 import com.example.myglovoapp.login_client.DataLoginClient;
 import com.example.myglovoapp.login_client.presenter.LoginClientPresenter;
+import com.example.myglovoapp.utils.ApiService;
+import com.example.myglovoapp.utils.RetrofitCliente;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LoginClientModel implements ContractLoginClient.Model{
     //aqui se interactua con el servidor
-    private static final String IP_BASE = "169.254.225.61:8080";
+    private static final String IP_BASE = "10.0.2.2:8080";
     private LoginClientPresenter presenter;
     public LoginClientModel(LoginClientPresenter presenter){
 
@@ -22,6 +28,20 @@ public class LoginClientModel implements ContractLoginClient.Model{
 
     @Override
     public void loginAPI(String email, String pass, OnLoginClientListener onLoginUserListener) {
+        ApiService apiService = RetrofitCliente.getClient("http://" + IP_BASE + "/GlovoBackend/").
+                create(ApiService.class);
 
+        Call<DataLoginClient> call = apiService.getDataUser ("CLIENT.LOGIN");
+        call.enqueue(new Callback<DataLoginClient>() {
+            @Override
+            public void onResponse(Call<DataLoginClient> call, Response<DataLoginClient> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<DataLoginClient> call, Throwable t) {
+
+            }
+        });
     }
 }
--- MyGlovoApp/app/src/main/java/com/example/myglovoapp/utils/ApiService.java
+++ MyGlovoApp/app/src/main/java/com/example/myglovoapp/utils/ApiService.java
@@ -1,8 +1,12 @@
 package com.example.myglovoapp.utils;
 
 
+import com.example.myglovoapp.login_client.DataLoginClient;
+
+import retrofit2.Call;
 import retrofit2.http.Headers;
+import retrofit2.http.Query;
 
 public interface ApiService {
-
+    Call<DataLoginClient> getDataUser(@Query("LOGIN") String login);
 }
