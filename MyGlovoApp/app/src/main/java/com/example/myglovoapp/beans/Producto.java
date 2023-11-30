8fedb4a7b616db56d67a8ce83d0feff6bd72addf
Moongara26569 <a26569@svalero.com>
2023-11-30 12:47:07 +0100

    dfds

--- MyGlovoApp/app/src/main/java/com/example/myglovoapp/beans/Producto.java
+++ MyGlovoApp/app/src/main/java/com/example/myglovoapp/beans/Producto.java
@@ -2,6 +2,7 @@ package com.example.myglovoapp.beans;
 
 public class Producto {
     private int idProducto;
+    private int idRestaurante;
     private String nombre;
     private double precio;
     private String desc;
